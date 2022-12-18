a6922475fbb42874f60ffa84c04ae0ce78588d7d
Mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2022-12-18 11:55:52 +0600

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,7 @@
-import { Dashboard } from './dashboard/dashboard'
+import Dashboard from './dashboard/dashboard'
 import { NextPage } from 'next'
 
 const DashboardPage: NextPage = (props) => {
 	return <Dashboard />
 }
-
-// export const getStaticProps: GetStaticProps = async () => {
-// 	try {
-// 		const { data: pages } = await pageService.fetchAllPages()
-// 		return {
-// 			props: {
-// 				pages
-// 			}
-// 		}
-// 	} catch (e) {
-// 		return {
-// 			props: {
-// 				pages: [] as IPage[]
-// 			}
-// 		}
-// 	}
-// }
-
 export default DashboardPage
