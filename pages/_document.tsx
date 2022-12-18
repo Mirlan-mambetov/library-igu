36eef751d913e7705b58b04af2711ec56a45f345
Mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2022-12-18 20:45:38 +0600

    changed document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,36 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import Document, {
+	Html,
+	Head,
+	Main,
+	NextScript,
+	DocumentContext
+} from 'next/document'
 
 class MyDocument extends Document {
-	static async getInitialProps(
-		ctx: DocumentContext
-	): Promise<DocumentInitialProps> {
+	static async getInitialProps(ctx: DocumentContext) {
 		const initialProps = await Document.getInitialProps(ctx)
+		return { ...initialProps }
+	}
 
-		return initialProps
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link
+						href='https://fonts.googleapis.com/css2?family=PT+Serif:wght@400;700&display=swap'
+						rel='stylesheet'
+					/>
+					<link
+						href='https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap'
+						rel='stylesheet'
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
 	}
 }
 
