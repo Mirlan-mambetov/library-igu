7172b3015ee7d7b1513fbe11a3a191d5f78a27c5
Mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2022-12-23 00:02:03 +0600

    global update

--- components/Arhivs/Arhivs.tsx
+++ components/Arhivs/Arhivs.tsx
@@ -1,28 +1,49 @@
+import { Button } from '../'
 import { Title } from '../Title/Title'
-// STYLES
 import styles from './Arhivs.module.scss'
 import { ArhivsProps } from './Arhivs.props'
+import classNames from 'classnames'
 import Link from 'next/link'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 export const Arhivs: FC<ArhivsProps> = ({ data, contentLink }): JSX.Element => {
+	const [more, setMore] = useState<boolean>(false)
 	const totalMaterials = data.flatMap((m) => m.materials).length
+
+	const moreHandler = () => {
+		if (!more) {
+			setMore(true)
+		} else {
+			setMore(false)
+		}
+	}
 	return (
 		<div className={styles.arhivs}>
 			<div className={styles.content}>
 				<Title type='h3'>Архивы вестника</Title>
 				<div className={styles.infoItem}>
-					<span>количество архивов: {data.length}</span>
+					<span>архивов: {data.length}</span>
 					<span>материалов: {totalMaterials}</span>
 				</div>
 			</div>
-			<div className={styles.links}>
+			<div
+				className={classNames(styles.links, {
+					[styles.more]: more
+				})}
+			>
 				{data.map((archiv) => (
 					<Link href={`/${contentLink}/arhiv/${archiv.id}`} key={archiv.id}>
 						<a>{archiv.name}</a>
 					</Link>
 				))}
 			</div>
+			<Button
+				style={{ marginTop: '20px' }}
+				orientation={more ? 'top' : 'bottom'}
+				onClick={moreHandler}
+			>
+				{!more ? 'Показать больше' : 'Показать меньше'}
+			</Button>
 		</div>
 	)
 }
--- components/Arhivs/Category/Category.tsx
+++ components/Arhivs/Category/Category.tsx
@@ -1,24 +1,48 @@
+import { Button } from '../../Button/Button'
 import { Title } from '../../Title/Title'
 // STYLES
 import styles from './Category.module.scss'
 import { CategoryProps } from './Category.props'
+import classNames from 'classnames'
 import Link from 'next/link'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 export const Category: FC<CategoryProps> = ({
 	arhivsLink,
 	data
 }): JSX.Element => {
+	const [more, setMore] = useState<boolean>(false)
+
+	const moreHandler = () => {
+		if (!more) {
+			setMore(true)
+		} else {
+			setMore(false)
+		}
+	}
 	return (
-		<div className={styles.category}>
-			<div className={styles.categoryTop}>
-				<Title type='h3'>Категории</Title>
+		<div className={styles.categories}>
+			<div
+				className={classNames(styles.category, {
+					[styles.more]: more
+				})}
+			>
+				<div className={styles.categoryTop}>
+					<Title type='h3'>Категории</Title>
+				</div>
+				{data?.map((arhiv) => (
+					<Link href={`/${arhivsLink}/arhiv/${arhiv.id}`} key={arhiv.id}>
+						<a>{arhiv.name}</a>
+					</Link>
+				))}
 			</div>
-			{data?.map((arhiv) => (
-				<Link href={`/${arhivsLink}/arhiv/${arhiv.id}`} key={arhiv.id}>
-					<a>{arhiv.name}</a>
-				</Link>
-			))}
+			<Button
+				style={{ marginTop: '20px' }}
+				orientation={more ? 'top' : 'bottom'}
+				onClick={moreHandler}
+			>
+				{!more ? 'Показать больше' : 'Показать меньше'}
+			</Button>
 		</div>
 	)
 }
