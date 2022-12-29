e634e62a429d8c1d17398348fc8e7194a33d4263
Mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2022-12-29 23:34:18 +0600

    update

--- pages/dashboard/faq/index.tsx
+++ pages/dashboard/faq/index.tsx
@@ -12,41 +12,13 @@ const FaqQuestions: NextPage = () => {
 		<Layout title='Информация об обновлении: Продукта'>
 			<Box color={colors.blueAccent[900]}>
 				<Box>
-					<Typography>Обновление: 28.12.2022 00:00</Typography>
+					<Typography>Обновление: 30.12.2022 00:00</Typography>
 					<Typography variant='inherit'>
-						Обновлений нет:
+						Частичное обновление:
 						<ul
 							style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}
 						>
-							<li style={{ color: `${colors.redAccent[600]}` }}>
-								Версия API(server): /api/v3/ будет обновлен в - - - -- : --
-								<ul
-									style={{
-										marginTop: '5px',
-										color: `${colors.redAccent[400]}`
-									}}
-								>
-									<li>Включает в себя:</li>
-									<li>Авторизацию в админ панель</li>
-									<li>
-										Страницы: <br />
-										Единое окно <br />
-										Кыргыз-тили <br />
-										Ссылки интернет <br />
-									</li>
-									<li>
-										Поисковики по сайту: <br />
-										Вестник: <br />
-										Труды преподавателей <br />
-										Электронная библиотека <br />
-										<span style={{ fontSize: '12px' }}>
-											Примечание: по админ панели поисковики не будут
-											реализованы. <br />
-											Вид поиска на панели останется для будущих реализаций
-										</span>
-									</li>
-								</ul>
-							</li>
+							<li>Доступен модуль Ссылки интернет</li>
 							<li style={{ color: `${colors.redAccent[600]}` }}>
 								Версия API(server): /api/v4/ будет обновлен в - - - -- : --
 								<ul
