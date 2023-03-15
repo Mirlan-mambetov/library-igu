dba689ad57d144b2e17d89f7117b58705c49c413
Prefect-m <Prefect-m>
2023-03-15 15:59:09 +0600

    change

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { Auth } from 'src/auth/decorators/auth.decorator';
+import { Controller, Get } from '@nestjs/common'
+import { UsersService } from './users.service'
 
 @Controller('users')
 export class UsersController {
 	constructor(private readonly userService: UsersService) {}
 
-
 	@Get()
-	@Auth('jwt')
 	getAllUsers() {
 		return this.userService.getAllUsers()
 	}
