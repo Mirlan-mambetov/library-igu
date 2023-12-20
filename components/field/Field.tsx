d7ad1791d5cb4db8039e005b5bf8d7e634c325df
mirlan-mambetov <prefect.mirlan@proton.me>
2023-12-20 22:01:18 +0600

    chagend display name

--- components/field/Field.tsx
+++ components/field/Field.tsx
@@ -1,10 +1,11 @@
 import { DetailsHTMLAttributes, InputHTMLAttributes, forwardRef } from 'react'
 
 interface IFieldComponentProps extends InputHTMLAttributes<HTMLInputElement> {}
+
 const FieldComponent = forwardRef<HTMLInputElement, IFieldComponentProps>(
   ({ type, className, ...props }, ref) => {
     return <input className={className} {...props} ref={ref} />
   }
 )
-
+FieldComponent.displayName = 'FieldComponent'
 export default FieldComponent
