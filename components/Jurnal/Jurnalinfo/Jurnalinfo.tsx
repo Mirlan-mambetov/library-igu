d7ca84fc9424e6d7551173b65eec668c8c11318c
mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2024-03-25 22:29:22 +0600

    update

--- components/Jurnal/Jurnalinfo/Jurnalinfo.tsx
+++ components/Jurnal/Jurnalinfo/Jurnalinfo.tsx
@@ -1,23 +1,23 @@
 import { Title } from '../../'
 import { IJournalHead } from '../../../interfaces/journal.interface'
 // STYLES
-import styles from './jurnalinfo.module.scss'
 import { FC } from 'react'
+import styles from './jurnalinfo.module.scss'
 
 export const Jurnalinfo: FC<IJournalHead> = ({ title, items }): JSX.Element => {
-	return (
-		<div className={styles.jurnalInfo}>
-			<Title className={styles.jurnalTitle} type='h3'>
-				{title}
-			</Title>
-			<div className={styles.wrapp}>
-				{items?.map((item) => (
-					<div className={styles.item} key={item.id}>
-						<span>{item.title}</span>
-						<span>{item.description}</span>
-					</div>
-				))}
-			</div>
-		</div>
-	)
+  return (
+    <div className={styles.jurnalInfo}>
+      <Title className={styles.jurnalTitle} type="h3">
+        {title}
+      </Title>
+      <div className={styles.wrapp}>
+        {items?.map((item) => (
+          <div className={styles.item} key={item.id}>
+            <span>{item.title}:</span>
+            <span>{item.description}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
--- pages/elibrary/index.tsx
+++ pages/elibrary/index.tsx
@@ -1,35 +1,35 @@
+import { NextSeo } from 'next-seo'
+import { FC } from 'react'
 import { Layout } from '../../Layout/Layout'
-import { Bookscard, ElibraryCategories, Hero, Title } from '../../components'
+import { ElibraryCategories, Hero } from '../../components'
 import {
-	IElibraryCategory,
-	IElibraryReamaining
+  IElibraryCategory,
+  IElibraryReamaining
 } from '../../interfaces/elibrary.interface'
 import { IPage } from '../../interfaces/page.interface'
 import { elibraryApi } from '../../store/api/elibrary/elibrary.api'
 import { pageApi } from '../../store/api/page/page.api'
 import styles from './elibrary.module.scss'
-import { NextSeo } from 'next-seo'
-import { FC } from 'react'
 
 const Elibrary: FC = (): JSX.Element => {
-	const pageId = 6
-	const { data: page = {} as IPage } = pageApi.useFetchPageQuery(pageId)
-	const { data: categories = [] as IElibraryCategory[] } =
-		elibraryApi.useFetchMainCategoriesQuery(null)
+  const pageId = 6
+  const { data: page = {} as IPage } = pageApi.useFetchPageQuery(pageId)
+  const { data: categories = [] as IElibraryCategory[] } =
+    elibraryApi.useFetchMainCategoriesQuery(null)
 
-	const { data: reamining = [] as IElibraryReamaining[] } =
-		elibraryApi.useFetchElibraryRemainingQuery(null)
+  const { data: reamining = [] as IElibraryReamaining[] } =
+    elibraryApi.useFetchElibraryRemainingQuery(null)
 
-	return (
-		<Layout>
-			<NextSeo
-				title='Электронная библиотека ИГУ - Научная библиотека ИГУ'
-				description='Электронная библиотека ИГУ Научная библиотека ИГУ'
-			/>
-			{/* Hero */}
-			{page.hero && page.hero.map((h) => <Hero data={h} key={h.id} />)}
-			{/* Remainings books */}
-			<section className={styles.remaining}>
+  return (
+    <Layout>
+      <NextSeo
+        title="Электронная библиотека ИГУ - Научная библиотека ИГУ"
+        description="Электронная библиотека ИГУ Научная библиотека ИГУ"
+      />
+      {/* Hero */}
+      {page.hero && page.hero.map((h) => <Hero data={h} key={h.id} />)}
+      {/* Remainings books */}
+      {/* <section className={styles.remaining}>
 				<div className='container'>
 					<div className={styles.remainignWrapp}>
 						<div className='sectionTitle'>
@@ -43,19 +43,19 @@ const Elibrary: FC = (): JSX.Element => {
 						</div>
 					</div>
 				</div>
-			</section>
-			{/* Categories */}
-			<section className={styles.categories}>
-				<div className='container'>
-					<ElibraryCategories
-						categoryTitle='Категории'
-						data={categories}
-						categoryLink='elibrary/category'
-					/>
-				</div>
-			</section>
-		</Layout>
-	)
+			</section> */}
+      {/* Categories */}
+      <section className={styles.categories}>
+        <div className="container">
+          <ElibraryCategories
+            categoryTitle="Категории"
+            data={categories}
+            categoryLink="elibrary/category"
+          />
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 export default Elibrary
