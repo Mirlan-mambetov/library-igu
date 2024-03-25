f943ff99610d8ef92102be10524cc5736842744b
mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2024-03-25 22:13:22 +0600

    updated

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,52 +1,65 @@
 // STYLES
-import classes from './Footer.module.scss'
 import { FC } from 'react'
+import classes from './Footer.module.scss'
 
 export const Footer: FC = (): JSX.Element => {
-	return (
-		<footer className={classes.footer}>
-			<div className='container'>
-				<div className={classes.footerColumn}>
-					<div className={classes.footerRow}>
-						<div className={classes.footerItem}>
-							Научная библиотека ИГУ им. К.Тыныстанова
-						</div>
-						<div className={classes.footerItem}>
-							Кыргызская Республика г.Каракол. ул.Абдрахманов 103/722200 (3922)
-							52180
-						</div>
-						<div className={classes.footerItem}>
-							<a
-								href='mailto:library@rambler.ru'
-								rel='noreferrer'
-								target='_blank'
-							>
-								library@rambler.ru
-							</a>
-						</div>
-						<div className={classes.footerItem}>
-							<a href='http://main.iksu.kg' rel='noreferrer' target='_blank'>
-								Сайт университета ИГУ
-							</a>
-						</div>
-					</div>
-					<div className={classes.footerRow}>
-						<div className={classes.footerItem}>Структура</div>
-						<div className={classes.footerItem}>
-							Режим работы: <span>08:00 - 17:00</span>
-						</div>
-						<div className={classes.footerItem}>
-							читальный зал № 1 <span>Тыныстанова 130 1-корпус 233</span>
-						</div>
-						<div className={classes.footerItem}>
-							читальный зал № 2 <span>Тыныстанова 130 1-корпус 233</span>
-						</div>
-						<div className={classes.footerItem}>
-							читальный зал № 3 <span>Тыныстанова 130 1-корпус 233</span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-	)
+  return (
+    <footer className={classes.footer}>
+      <div className="container">
+        <div className={classes.footerColumn}>
+          <div className={classes.footerRow}>
+            <div className={classes.footerItem}>
+              Научная библиотека ИГУ им. К.Тыныстанова
+            </div>
+            <div className={classes.footerItem}>
+              <span>Адрес:</span>
+              722200, Кыргызская Республика, г.Каракол, ул.Гебзе 120.
+            </div>
+            <div className={classes.footerItem}>
+              <span>Тел: </span>
+              <a href="tel:(3922)52180">(3922) 52180</a>
+            </div>
+            <div className={classes.footerItem}>
+              <span>E-mail</span>
+              <a
+                href="mailto:library@rambler.ru"
+                rel="noreferrer"
+                target="_blank"
+              >
+                igulibrary@rambler.ru
+              </a>
+            </div>
+            <div className={classes.footerItem}>
+              <span>Веб сайт университета:</span>
+              <a href="http://main.iksu.kg" rel="noreferrer" target="_blank">
+                main.iksu.kg
+              </a>
+            </div>
+          </div>
+          <div className={classes.footerRow}>
+            <div className={classes.footerItem}>Структура библиотеки</div>
+            <div className={classes.footerItem}>
+              Режим работы: <span>08:00 - 17:00</span>
+            </div>
+            <div className={classes.footerItem}>
+              Абонемент <span>3 корпус, 216</span>
+            </div>
+
+            <div className={classes.footerItem}>
+              Читальный зал № 1 <span>1 корпус, 217</span>
+            </div>
+            <div className={classes.footerItem}>
+              Читальный зал № 2 <span>3 корпус, 218</span>
+            </div>
+            <div className={classes.footerItem}>
+              Читальный зал № 3 <span>4 корпус, 105</span>
+            </div>
+            <div className={classes.footerItem}>
+              Зал электронной документации <span>3 корпус, 109</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
--- pages/news/[id].tsx
+++ pages/news/[id].tsx
@@ -1,75 +1,75 @@
-import { Layout } from '../../Layout/Layout'
-import { Button, Hero, Imagebox, Title, NewsComponent } from '../../components'
-import { INews } from '../../interfaces/news.interface'
-import { newsService } from '../../services/newsService/newsService'
-import styles from './news.module.scss'
 import { GetServerSideProps } from 'next'
 import { NextSeo } from 'next-seo'
 import Link from 'next/link'
 import { FC } from 'react'
+import { Layout } from '../../Layout/Layout'
+import { Button, Imagebox, NewsComponent, Title } from '../../components'
+import { INews } from '../../interfaces/news.interface'
+import { newsService } from '../../services/newsService/newsService'
+import styles from './news.module.scss'
 
 interface NewsI {
-	news: INews
-	newses: INews[]
+  news: INews
+  newses: INews[]
 }
 
 const NewsPage: FC<NewsI> = ({ news, newses }) => {
-	const currentNews = newses.filter((n) => n.id !== news.id)
-	return (
-		<Layout>
-			<NextSeo
-				title={`${news.title} - Научная библиотека ИГУ`}
-				description={`Научная библиотека ИГУ Вестник ИГУ им. К. Тыныстанова ${news.title} - ${news.description}`}
-			/>
-			{/* Hero */}
-			<Hero data={{ ...news }} />
-			{/* News description */}
-			<section className={styles.description}>
-				<div className='container'>
-					<Imagebox data={{ ...news }} />
-				</div>
-			</section>
-			{/* News cards */}
-			<section className={styles.cards}>
-				<div className='container'>
-					<div className='sectionTitle'>
-						<Title type='h3'>Читайте также</Title>
-					</div>
-					<div className={styles.cardsWrapp}>
-						<NewsComponent data={currentNews} />
-					</div>
-					<div className={styles.cardsBtn}>
-						<Link href={'/news/arhiv'}>
-							<a>
-								<Button>Архив новостей</Button>
-							</a>
-						</Link>
-					</div>
-				</div>
-			</section>
-		</Layout>
-	)
+  const currentNews = newses.filter((n) => n.id !== news.id)
+  return (
+    <Layout>
+      <NextSeo
+        title={`${news.title} - Научная библиотека ИГУ`}
+        description={`Научная библиотека ИГУ Вестник ИГУ им. К. Тыныстанова ${news.title} - ${news.description}`}
+      />
+      {/* Hero */}
+      {/* <Hero data={{ ...news }} /> */}
+      {/* News description */}
+      <section className={styles.description}>
+        <div className="container">
+          <Imagebox data={{ ...news }} />
+        </div>
+      </section>
+      {/* News cards */}
+      <section className={styles.cards}>
+        <div className="container">
+          <div className="sectionTitle">
+            <Title type="h3">Читайте также</Title>
+          </div>
+          <div className={styles.cardsWrapp}>
+            <NewsComponent data={currentNews} />
+          </div>
+          <div className={styles.cardsBtn}>
+            <Link href={'/news/arhiv'}>
+              <a>
+                <Button>Архив новостей</Button>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-	try {
-		// @ts-ignore
-		const { data: news } = await newsService.findNewsById(+params?.id)
-		const { data: newses } = await newsService.findNewsOnLimit(6)
-		return {
-			props: {
-				news,
-				newses
-			}
-		}
-	} catch (e) {
-		return {
-			props: {
-				news: {} as INews,
-				newses: [] as INews[]
-			} as NewsI,
-			notFound: true
-		}
-	}
+  try {
+    // @ts-ignore
+    const { data: news } = await newsService.findNewsById(+params?.id)
+    const { data: newses } = await newsService.findNewsOnLimit(6)
+    return {
+      props: {
+        news,
+        newses
+      }
+    }
+  } catch (e) {
+    return {
+      props: {
+        news: {} as INews,
+        newses: [] as INews[]
+      } as NewsI,
+      notFound: true
+    }
+  }
 }
 
 export default NewsPage
